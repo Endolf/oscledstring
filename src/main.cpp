0a324cc860413952494942649eab23d3c34a398f
Endolf <endolf@computerbooth.com>
2020-01-01 22:39:27 +0000

    Add Open Sound Control and first message processing.

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,20 @@
 #include "main.h"
 #include <WiFi.h>
 #include <WiFiAP.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
+
+static const char buildInfoLogString[] = "Build data: %s, %s %s";
 
 const char ssid[] = "OSCNet";
 const char password[] = "oscnet1324";
 
-static const char buildInfoLogString[] = "Build data: %s, %s %s";
 char logBuffer[256];
 
-WiFiServer server(80);
+WiFiUDP Udp;
+OSCErrorCode error;
 
 void setup()
 {
@@ -21,10 +27,45 @@ void setup()
     sprintf(logBuffer, "OSCNet server IP: %s", myIP.toString().c_str());
     Serial.println(logBuffer);
 
-    server.begin();
-    Serial.println("Server started");
+    Udp.begin(OSCPORT);
+    sprintf(logBuffer, "OSC Listening on port %d", OSCPORT);
+    Serial.println(logBuffer);
+}
+
+void solidColour(OSCMessage &msg)
+{
+    uint32_t colourAsInt = msg.getInt(0);
+    uint8_t r,g,b,a;
+    r = (colourAsInt >> 24) & 255;
+    g = (colourAsInt >> 16) & 255;
+    b = (colourAsInt >> 8) & 255;
+    a = colourAsInt & 255;
+
+    sprintf(logBuffer, "Got solid colour request for 0x%2.2X%2.2X%2.2x%2.2X - (%u)", r, g, b, a, colourAsInt);
+    Serial.println(logBuffer);
 }
 
 void loop()
 {
+    //OSCBundle bundle;
+    OSCMessage msg;
+    int size = Udp.parsePacket();
+
+    if (size > 0)
+    {
+        while (size--)
+        {
+            msg.fill(Udp.read());
+        }
+        if (!msg.hasError())
+        {
+            msg.dispatch("/solid", solidColour);
+        }
+        else
+        {
+            error = msg.getError();
+            Serial.print("error: ");
+            Serial.println(error);
+        }
+    }
 }
--- src/main.h
+++ src/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define OSCPORT 2000
+
 void setup();
 void loop();
 
