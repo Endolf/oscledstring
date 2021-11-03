7b850314a77758b9985f50ade7edcf52ac1287ce
Endolf <endolf@computerbooth.com>
2021-11-03 15:27:08 +0000

    Update pin, led count and fps

--- src/main.h
+++ src/main.h
@@ -5,12 +5,12 @@
 #define FASTLED_INTERNAL
 
 #define OSCPORT 2000
-#define LED_DATA_PIN 2
+#define LED_DATA_PIN 25
 #define LED_TYPE WS2813
 #define COLOR_ORDER GRB
-#define NUM_LEDS 144
+#define NUM_LEDS 300
 
-#define FRAMES_PER_SECOND 120
+#define FRAMES_PER_SECOND 60
 
 void setup();
 void loop();
